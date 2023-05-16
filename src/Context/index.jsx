018757cd75be1fa7ced42046ebf635c57c2583d0
Ingrid G <ing.gape18@gmail.com>
2023-05-16 13:21:35 -0500

    pages y router

---
+++ src/Context/index.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from 'react'
+//estado global de la aplicacion-fuente de verdad a donde vamos a hacer request desde dif componentes de la app
+//1 crear un contexto
+export const CartContext = createContext()
+
+//el context se provee mediante un provideer
+export const CartContextProvider = ({ children }) => {
+
+    //Shopping Cart Counter - Qty
+    const [count, setCount] = useState(0)
+    //console.log('count' + count)
+
+
+    return (
+    //llamamos al proveedor del contexto y hacemos un wrapper - con el q provee la info del contexto
+    //2 crear un proveedor que va a encapsular todos los componentes que tenemos en App para proveerlos de informacion
+        //va a tener un hijo(App) que necesita que lea count y tb que cualquier componente puede modificar ese valor
+        <CartContext.Provider value={{
+            count, 
+            setCount,
+            
+        }}>
+            { children }    
+        </CartContext.Provider>   
+    )
+}
---
+++ src/Pages/Error404/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+const Error404 = () => { 
+    return (
+        <div>
+            <p>Pagina no encontrada</p>
+        </div>
+        
+    )
+}
+
+export default Error404
